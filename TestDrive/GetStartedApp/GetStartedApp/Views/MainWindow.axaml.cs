6e04cee0eddda0a197700c96ac99f63c55248342
zrq <wyzrq163@163.com>
2024-10-13 20:46:11 +0800

    小试牛刀

--- TestDrive/GetStartedApp/GetStartedApp/Views/MainWindow.axaml.cs
+++ TestDrive/GetStartedApp/GetStartedApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace GetStartedApp.Views;
 
@@ -7,5 +10,23 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (Double.TryParse(Celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            Fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            Celsius.Text = "0";
+            Fahrenheit.Text = "0";
+        }
+        Console.WriteLine("Clicked");
+        Debug.WriteLine($"Click! Celsius={this.Celsius.Text}");
+        return;
+        throw new System.NotImplementedException();
     }
 }
