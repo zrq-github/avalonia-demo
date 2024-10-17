955f22955b6355c2925b3caaa86ceb48021a6eeb
zrq <wyzrq163@163.com>
2024-10-17 21:48:48 +0800

    Avalonia.MusicStore

---
+++ TestDrive/Avalonia.MusicStore/Avalonia.MusicStore/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Avalonia.MusicStore.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ TestDrive/Avalonia.MusicStore/Avalonia.MusicStore/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.MusicStore.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
