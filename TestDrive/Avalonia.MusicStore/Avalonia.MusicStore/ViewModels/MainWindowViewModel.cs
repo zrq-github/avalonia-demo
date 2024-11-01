d1b0479344257ed92f1d364482df86bf29eb9c8a
zrq <wyzrq163@163.com>
2024-11-02 00:37:02 +0800

    按钮命令

--- TestDrive/Avalonia.MusicStore/Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ TestDrive/Avalonia.MusicStore/Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,21 @@
-﻿namespace Avalonia.MusicStore.ViewModels;
+﻿using System.Windows.Input;
+using ReactiveUI;
+
+namespace Avalonia.MusicStore.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+    
+    public ICommand BuyMusicCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        BuyMusicCommand = ReactiveCommand.Create(() =>
+        {
+
+        });
+    }
 }
