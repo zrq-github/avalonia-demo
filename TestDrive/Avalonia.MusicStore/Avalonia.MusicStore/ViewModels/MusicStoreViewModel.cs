20869fb3bcbd6e2f18d003f474842a86ce1e45ec
zrq <wyzrq163@163.com>
2024-11-02 11:04:35 +0800

    模拟搜索

--- TestDrive/Avalonia.MusicStore/Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
+++ TestDrive/Avalonia.MusicStore/Avalonia.MusicStore/ViewModels/MusicStoreViewModel.cs
@@ -1,6 +1,45 @@
-﻿namespace Avalonia.MusicStore.ViewModels;
+﻿using System.Collections.ObjectModel;
+using ReactiveUI;
 
-public class MusicStoreViewModel
+namespace Avalonia.MusicStore.ViewModels;
+
+public class MusicStoreViewModel: ViewModelBase
 {
+    private string? _searchText;
+    private bool _isBusy;
+    private AlbumViewModel? _selectedAlbum;
+
+    public MusicStoreViewModel()
+    {
+        // 数据模拟
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+        SearchResults.Add(new AlbumViewModel());
+    }
     
+    public ObservableCollection<AlbumViewModel> SearchResults { get; } = new();
+    
+    public AlbumViewModel? SelectedAlbum
+    {
+        get => _selectedAlbum;
+        set => this.RaiseAndSetIfChanged(ref _selectedAlbum, value);
+    }
+    
+    /// <summary>
+    /// 搜过字符串
+    /// </summary>
+    public string? SearchText
+    {
+        get => _searchText;
+        set => this.RaiseAndSetIfChanged(ref _searchText, value);
+    }
+
+    /// <summary>
+    /// 是否正在查询
+    /// </summary>
+    public bool IsBusy
+    {
+        get => _isBusy;
+        set => this.RaiseAndSetIfChanged(ref _isBusy, value);
+    }
 }
