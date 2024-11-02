5306373e56ec1464b2593c7e76fcdd547f0b92ef
zrq <wyzrq163@163.com>
2024-11-02 17:41:26 +0800

    测试跨平台

---
+++ src/Avalonia.MusicStore/Avalonia.MusicStore/Avalonia.MusicStore/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Avalonia.MusicStore.ViewModels;
+
+public abstract class ViewModelBase : ReactiveObject
+{
+}
---
+++ src/Avalonia.MusicStore/Avalonia.MusicStore/Avalonia.MusicStore/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.MusicStore.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
