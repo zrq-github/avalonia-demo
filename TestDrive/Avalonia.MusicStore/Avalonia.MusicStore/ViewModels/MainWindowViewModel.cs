fcdb0b52347fa1821e1dfa6b62c58914357d325d
zrq <wyzrq163@163.com>
2024-11-02 15:25:12 +0800

    将项目添加到用户的集合中

--- TestDrive/Avalonia.MusicStore/Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ TestDrive/Avalonia.MusicStore/Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Reactive.Linq;
+﻿using System.Collections.ObjectModel;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using ReactiveUI;
 
@@ -10,17 +11,18 @@ public class MainWindowViewModel : ViewModelBase
     {
         ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel?>();
 
-        BuyMusicCommand = ReactiveCommand.Create(async () =>
+        BuyMusicCommand = ReactiveCommand.CreateFromTask(async () =>
         {
             var store = new MusicStoreViewModel();
-
             var result = await ShowDialog.Handle(store);
+            if (result != null) Albums.Add(result);
         });
     }
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
 
+    public ObservableCollection<AlbumViewModel> Albums { get; } = new();
     public ICommand BuyMusicCommand { get; }
 
     public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
