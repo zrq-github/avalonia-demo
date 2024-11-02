380839eacbbb3a3e545bb9781c66c5d724dc6f03
zrq <wyzrq163@163.com>
2024-11-02 16:24:58 +0800

    跨平台基础

---
+++ src/Avalonia.CrossPlatform.MusicStore/Avalonia.CrossPlatform.MusicStore/Avalonia.CrossPlatform.MusicStore/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Avalonia.CrossPlatform.MusicStore.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+}
---
+++ src/Avalonia.CrossPlatform.MusicStore/Avalonia.CrossPlatform.MusicStore/Avalonia.CrossPlatform.MusicStore/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Avalonia.CrossPlatform.MusicStore.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
